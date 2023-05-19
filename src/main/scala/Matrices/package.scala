047e46415c678329cc04124411d8f35aae96ddfd
JuanDavid <juan.loaiza.santiago@correounivalle.edu.co>
2023-05-19 09:54:30 -0500

    Tipo de paquete actualizado

--- src/main/scala/Matrices/package.scala
+++ src/main/scala/Matrices/package.scala
@@ -1,6 +1,6 @@
 import scala.util.Random
 
-object Matrices {
+package object Matrices {
   val random = new Random()
   type Matriz = Vector[Vector[Int]]
 
@@ -20,53 +20,53 @@ object Matrices {
     (v1 zip v2).map { case (i, j) => i * j }.sum
   }
 
-//    // Ejercicio 1.1.1
-//    def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
-//    }
-//
-//    // Ejercicio 1.1.2
-//    def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
-//    }
-//
-//    // Ejercicio 1.2.1
-//    def subMatriz(m: Matriz, i: Int, j: Int, l: Int): Matriz = {
-//      // Dada m, matriz cuadrada de NxN, 1<=i, j<=N, i+n<=N, j+n<=N,
-//      // devuelve la submatriz de nxn correspondiente a m[i..i+(n−1), j..j+(n−1)]
-//    }
-//
-//    // Ejercicio 1.2.2
-//    def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
-//      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
-//      // y devuelve la suma de las 2 matrices
-//    }
-//
-//    // Ejercicio 1.2.3
-//    def multMatrizRec(m1: Matriz, m2: Matriz): Matriz = {
-//      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
-//      // y devuelve la multiplicación de las 2 matrices
-//    }
-//
-//    // Ejercicio 1.2.4
-//    def multMatrizRecPar(m1: Matriz, m2: Matriz): Matriz = {
-//      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
-//      // y devuelve la multiplicación de las 2 matrices, en paralelo
-//    }
-//
-//    // Ejercicio 1.3.1
-//    def restaMatriz(m1: Matriz, m2: Matriz): Matriz = {
-//      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
-//      // y devuelve la resta de las 2 matrices
-//    }
-//
-//    // Ejercicio 1.3.2
-//    def multStrassen(m1: Matriz, m2: Matriz): Matriz = {
-//      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
-//      // y devuelve la multiplicación de las 2 matrices usando el algoritmo de Strassen
-//    }
-//
-//    // Ejercicio 1.3.3
-//    def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
-//      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
-//      // y devuelve la multiplicación en paralelo de las 2 matrices usando el algoritmo de Strassen
-//    }
+  //    // Ejercicio 1.1.1
+  //    def multMatriz(m1: Matriz, m2: Matriz): Matriz = {
+  //    }
+  //
+  //    // Ejercicio 1.1.2
+  //    def multMatrizPar(m1: Matriz, m2: Matriz): Matriz = {
+  //    }
+  //
+  //    // Ejercicio 1.2.1
+  //    def subMatriz(m: Matriz, i: Int, j: Int, l: Int): Matriz = {
+  //      // Dada m, matriz cuadrada de NxN, 1<=i, j<=N, i+n<=N, j+n<=N,
+  //      // devuelve la submatriz de nxn correspondiente a m[i..i+(n−1), j..j+(n−1)]
+  //    }
+  //
+  //    // Ejercicio 1.2.2
+  //    def sumMatriz(m1: Matriz, m2: Matriz): Matriz = {
+  //      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
+  //      // y devuelve la suma de las 2 matrices
+  //    }
+  //
+  //    // Ejercicio 1.2.3
+  //    def multMatrizRec(m1: Matriz, m2: Matriz): Matriz = {
+  //      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
+  //      // y devuelve la multiplicación de las 2 matrices
+  //    }
+  //
+  //    // Ejercicio 1.2.4
+  //    def multMatrizRecPar(m1: Matriz, m2: Matriz): Matriz = {
+  //      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
+  //      // y devuelve la multiplicación de las 2 matrices, en paralelo
+  //    }
+  //
+  //    // Ejercicio 1.3.1
+  //    def restaMatriz(m1: Matriz, m2: Matriz): Matriz = {
+  //      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
+  //      // y devuelve la resta de las 2 matrices
+  //    }
+  //
+  //    // Ejercicio 1.3.2
+  //    def multStrassen(m1: Matriz, m2: Matriz): Matriz = {
+  //      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
+  //      // y devuelve la multiplicación de las 2 matrices usando el algoritmo de Strassen
+  //    }
+  //
+  //    // Ejercicio 1.3.3
+  //    def multStrassenPar(m1: Matriz, m2: Matriz): Matriz = {
+  //      // recibe m1 y m2 matrices cuadradas del mismo tamaño, potencia de 2
+  //      // y devuelve la multiplicación en paralelo de las 2 matrices usando el algoritmo de Strassen
+  //    }
 }
